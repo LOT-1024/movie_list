414d7a20385fe0ae74968e0739abeb9ce91b258f
LOT-1024 <tunggultanvan@gmail.com>
2024-09-10 20:32:49 +0700

    update

---
+++ src/components/loading-spinner.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+interface LoadingSpinnerProps extends React.HTMLAttributes<HTMLDivElement> {
+    size?: "sm" | "md" | "lg";
+}
+
+const LoadingSpinner = React.forwardRef<HTMLDivElement, LoadingSpinnerProps>(
+    ({ className, size = "md", ...props }, ref) => {
+        const sizeClasses = {
+            sm: "w-5 h-5 border-2",
+            md: "w-8 h-8 border-3",
+            lg: "w-12 h-12 border-4",
+        };
+
+        return (
+            <div
+                ref={ref}
+                className={cn(
+                    "inline-block animate-spin rounded-full border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]",
+                    sizeClasses[size],
+                    className
+                )}
+                role="status"
+                aria-label="loading"
+                {...props}
+            >
+                <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                    Loading...
+                </span>
+            </div>
+        );
+    }
+);
+LoadingSpinner.displayName = "LoadingSpinner";
+
+export { LoadingSpinner };
