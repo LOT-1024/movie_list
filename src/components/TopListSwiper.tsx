28d1d6bfd165880f71242d7105cf551826c091c9
LOT-1024 <tunggultanvan@gmail.com>
2024-09-11 20:57:27 +0700

    fix module

--- src/components/TopListSwiper.tsx
+++ src/components/TopListSwiper.tsx
@@ -1,6 +1,6 @@
 "use client";
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
